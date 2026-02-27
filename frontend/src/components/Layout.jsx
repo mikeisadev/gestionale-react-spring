75d3f200d1fe5a5f5eac7adac985f28c5342d36e
Michele <michelework02@gmail.com>
2026-02-27 18:44:30 +0100

    aggiunta dei commenti

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,6 +4,8 @@ const Layout = () => {
 
     return (
         <div id="main" className='flex'>
+
+            {/** SIDEBAR CON IL MENU DEL GESTIONALE */}
             <div id="sidebar" className='p-4 bg-blue-300 fixed h-full w-[320px]'>
                 <div className="navigation-bar flex flex-col gap-4">
                     <Link to="/prodotti">Prodotti</Link>
@@ -12,6 +14,11 @@ const Layout = () => {
                     <Link to="/progetti">Progetti</Link>
                 </div>
             </div>
+
+            {/** 
+                CONTENT - OUTLET serve a renderizzare il contenuto 
+                delle rotte interne al layout che si trova in una rotta "padre" 
+            */}
             <div id="content" className=''>
                 <Outlet />
             </div>
