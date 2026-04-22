bb97218c5ef01c9f3eba115134c0b8b7ee38a247
Michele <michelework02@gmail.com>
2026-04-22 16:22:56 +0200

    force changes

--- frontend/src/pages/Categorie.jsx
+++
@@ -1,8 +0,0 @@
-function Categorie() {
-
-    return (
-        <h1>Questa è la pagina categorie</h1>
-    );
-}
-
-export default Categorie;
--- frontend/src/pages/Dipendenti.jsx
+++
@@ -1,8 +0,0 @@
-function Dipendenti() {
-
-    return (
-        <h1>Questa è la pagina Dipendenti</h1>
-    );
-}
-
-export default Dipendenti;
